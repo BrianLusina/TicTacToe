c8ee93e3f906b51e380b1f53e019e6674b586a8d
BrianLusina <lusinabrian@gmail.com>
2017-01-09 22:20:29 +0300

    update body color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,10 @@ export default class Footer extends Component{
             <footer>
                 <div className="row">
                     <div className="small-12 columns">
-                        <p>Made by <a href="http://thelusina.surge.sh" target="_blank">The Lusina</a> #Gamer <i className="fa fa-facebook"></i></p>
+                        <p>Made by 
+                            <a href="http://thelusina.surge.sh" target="_blank"> The Lusina  </a>
+                            #Gamer
+                        </p>
                     </div>
                 </div>
             </footer>

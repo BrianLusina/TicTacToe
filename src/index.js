95c239a5d2ba109b15ad2172e06731f3860cd511
BrianLusina <lusinabrian@gmail.com>
2017-01-09 22:11:44 +0300

    change domain name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
 import './styles/reset.css';
-import './styles/font-awesome/font-awesome.min.css';
+import './styles/font-awesome/css/font-awesome.min.css';
 import './styles/foundation/foundation.min.css';
 import './styles/sweetalert.css';
 
